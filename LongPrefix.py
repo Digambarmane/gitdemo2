7c87ab814aea604f9da2b4c6df5e087f42e51d3d
mane <digambar6191@gmail.com>
2023-01-28 17:28:09 +0530

    at line 4 ,give bracket at last at line 9 ,print out
    write remaining program

--- LongPrefix.py
+++ LongPrefix.py
@@ -1,8 +1,20 @@
 def long(strs):
-   minle=strs[0]
+   if len(strs)==0:
+       return ""
+   minle = len(strs[0])
    for i in range(len(strs)):
        minle=min(minle,len(strs[i]))
-   
+
+   lcp=""
+   i=0
+   while i<=minle:
+       char=strs[0][i]
+       for j in range (1,len(strs)):
+           if strs[j][i]!=char:
+               return lcp
+       lcp=lcp+char
+       i=i+1
+   return lcp
 
 strs=["flower","flow","fly"]
 out=long(strs)
